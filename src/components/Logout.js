e2ba78607f747a76126456fc42659e4e3230e25d
wsigma21 <wallstlbl@gmail.com>
2024-01-01 14:41:19 +0900

    feat: ログアウト機能

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { auth } from "../firebase";
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    // ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    })
+  }
   return (
-    <div>Logout</div>
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
   )
 }
 
