4d2f6bf67ee24b2245191b2993500cf660f2e41c
wsigma21 <wallstlbl@gmail.com>
2024-01-01 16:08:41 +0900

    feat: Homeコンポーネント作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Sigma</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
