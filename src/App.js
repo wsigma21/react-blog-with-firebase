f814c08e2acd1435fb2180294ab17ba2628af4be
wsigma21 <wallstlbl@gmail.com>
2024-01-01 14:36:20 +0900

    feat: ログイン機能実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import CreatePost from './components/CreatePost';
 import Login from './components/Login';
@@ -7,13 +8,15 @@ import Logout from './components/Logout';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      // 認証状態をローカルストレージに保持
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+
+      // Homeへ遷移
+      navigate("/");
+    });
+  }
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
