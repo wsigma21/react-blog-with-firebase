01f34d1230b28fb1d7da90f19f0d0ed217733274
wsigma21 <wallstlbl@gmail.com>
2024-01-01 15:05:00 +0900

    feat: ブログ投稿ページにCSSを適用

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./CreatePost.css"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入"/>
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入"></textarea>
+        </div>
+        <button className="postButton">投稿する</button>
+      </div>
+    </div>
   )
 }
 
