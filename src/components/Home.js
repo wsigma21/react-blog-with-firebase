b03bf4576cf6e5643b81ce9226689674e5b97035
wsigma21 <wallstlbl@gmail.com>
2024-01-01 16:40:03 +0900

    feat: 記事を画面に反映

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from "firebase/firestore";
 import "./Home.css"
+import { db } from '../firebase';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+  // マウント時にデータ取得
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getPosts();
+  }, []);
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
+      {postList.map((post) => (
+        <div className="postContents" key={post.id}>
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">
+            {post.postsText}
+          </div>
+          <div className="nameAndDeleteButton">
+            <h3>@{post.author.username}</h3>
+            <button>削除</button>
+          </div>
         </div>
-        <div className="postTextContainer">
-          React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜React頑張るぞ〜〜〜〜〜〜〜〜〜〜〜〜
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@Sigma</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
